2fea0b4d32023d9b089e190bca4f1e3838ec07b9
Laviel <fafansin@gmail.com>
2020-09-30 09:11:30 +0300

    include color box component

--- src/Palette.js
+++ src/Palette.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import ColorBox from './ColorBox';
+import "./Palette.css";
 
 export default class Palette extends Component{
     render(){
         return (
             <div className="Palette">
                 <div className="Palette-colors">
-                    
+                    {this.props.colors.map((color) => (<ColorBox key={color.name} background={color.color} name={color.name} />))}
                 </div>
             </div>
         )

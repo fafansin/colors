1cd2a54d73c6f50178bc46a70489e480d1319db9
Laviel <fafansin@gmail.com>
2020-10-01 11:32:04 +0300

    palette route

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import seedColors from './seedColors';
 import {generatePalette} from "./colorHelpers";
 import {Route, Switch} from 'react-router-dom';
 
+
+function findPalette(id){
+    return seedColors.find(function(palette){
+        return palette.id === id;
+    })
+}
+
 function App(){
     return (
-        
-            <Switch>
-                <Route exact path="/" render={() => <h1>Testing</h1>}/>
-                <Route exact path="/palette/:id" render={() => <h1>Individual ID</h1>}/>
-            </Switch>
-        // <div>
-        //     <Palette palette={generatePalette(seedColors[4])}/>
-        // </div>
+        <Switch>
+            <Route exact path="/" render={() => <h1>Testing</h1>}/>
+            <Route exact path="/palette/:id" 
+                render={(routeProps) => {
+                        return <Palette palette={generatePalette(findPalette(routeProps.match.params.id))}/>
+                    }}
+            />
+        </Switch>
     );
 }
 
